4adfc1657d2aeb7999d7d2006ed97ff8e7d6431b
Sam Almeida <samRAlmeida@icloud.com>
2017-10-11 19:29:46 -0400

    initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,17 +6,14 @@ $(".chooseChakra").click(function(){
 	$(".chooseChakra").animate({
 		opacity: "0"})
 		
-	$(".firstChakras").animate({
-		opacity: "1"})
+	$(".firstChakras").animate({opacity: "1"})
 })
 
 $(".firstRoot").click(function(){
 	$(".firstChakras").animate({opacity: "0"})
-	$(".firstRoot").animate({opacity: "1"},{
-		bottom: "200px"
-	})
+	$(".rootImage").animate({opacity: "1"})
+	$(".rootImage").css("animation", "rotate 999s");
 })
-
 
 
 
