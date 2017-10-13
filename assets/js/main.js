a82d7251afc490a492ea745375274bfd387e4ccb
Sam Almeida <samRAlmeida@icloud.com>
2017-10-13 13:19:45 -0400

    all chakras mvp

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,49 @@
 $("document").ready(function(){
-//---------------------------------------------------------------
+//------------------------------------------------------------------------------------//
+$(".min5").on("touchstart click", function(){
+	$(".min5").fadeOut(100);
+	$(".min15").fadeOut(350);
+	$(".min30").fadeOut(700, function(){
+		$(".timerDisplay").fadeIn();
+//----------------------------------------//
+		min5Countdown();
+//----------------------------------------//
+		function stopInterval(){
+			clearInterval(min5StopInverval)
+		}	
+//----------------------------------------//
+	});
+})
+//------------------------------------------------------------------------------------//
+$(".min15").on("touchstart click", function(){
+	$(".min5").fadeOut(100);
+	$(".min15").fadeOut(350);
+	$(".min30").fadeOut(700, function(){
+		$(".timerDisplay").fadeIn();
+//----------------------------------------//
+			min15Countdown();
+//----------------------------------------//
+		function stopInterval(){
+			clearInterval(min5StopInverval)
+		}	
+//----------------------------------------//
+	});
+})
+//------------------------------------------------------------------------------------//
+$(".min30").on("touchstart click", function(){
+	$(".min5").fadeOut(100);
+	$(".min15").fadeOut(350);
+	$(".min30").fadeOut(700, function(){
+		$(".timerDisplay").fadeIn();
+//----------------------------------------//
+		min30Countdown();
+//----------------------------------------//
+		function stopInterval(){
+			clearInterval(min5StopInverval)
+		}	
+	});
+})
+//------------------------------------------------------------------------------------//
 function min5Countdown(){
 		var m = 5;
 		var s = 0;
@@ -36,7 +80,7 @@ function min5Countdown(){
     	}	
 	},1000)
 }
-//---------------------------------------------------------------//
+//------------------------------------------------------------------------------------//
 function min15Countdown(){
 	var m = 15;
 			var s = 0;
@@ -91,7 +135,7 @@ function min15Countdown(){
 			stopInterval();
 		}
 		s = s -= 1	
-//--------------------------------------------
+//----------------------------------------//
 		setTimeout(function(){
 			o.disconnect(ctx.destination);
 		},899000)
@@ -104,7 +148,7 @@ function min15Countdown(){
     	}	
 	},1000)
 }
-//---------------------------------------------------------------//
+//------------------------------------------------------------------------------------//
 function min30Countdown(){
 		var m = 30;
 			var s = 0;
@@ -204,7 +248,7 @@ function min30Countdown(){
 			stopInterval();
 		}
 		s = s -= 1	
-//--------------------------------------------
+//----------------------------------------//
 		setTimeout(function(){
 			o.disconnect(ctx.destination);
 		},1799000)
@@ -217,7 +261,7 @@ function min30Countdown(){
     	}	
 	},1000)
 }
-//---------------------------------------------------------------//
+//------------------------------------------------------------------------------------//
 if('webkitAudioContext' in window) {
     var myAudioContext = new webkitAudioContext();
 }
@@ -239,7 +283,7 @@ var o = ctx.createOscillator();
 o.start(0.5);
 o.type = "sine";
 g = ctx.createGain();
-//---------------------------------------------------------------//
+//------------------------------------------------------------------------------------//
 $(".chooseChakra").on("touchstart click", function(){
 	$(".chooseChakra").fadeOut(100, function(){
 		$(".firstRoot").fadeIn(0);
@@ -251,7 +295,7 @@ $(".chooseChakra").on("touchstart click", function(){
 		$(".firstCrown").fadeIn(700);
 	})
 });
-//---------------------------------------------------------------
+//------------------------------------------------------------------------------------//
 $(".firstRoot").on("touchstart click", function(){
 	$(".rootImage").css("animation", "rotate 999s");
 		$(".firstChakras").animate({opacity: "0"}, function(){
@@ -261,7 +305,7 @@ $(".firstRoot").on("touchstart click", function(){
 				$(".f912").fadeIn(700);
 	});
 })
-//---------------------------------------------------------------//
+//----------------------------------------//
 $(".f228").on("touchstart click", function(){
 	o.frequency.value = 303;
 	o.connect(ctx.destination);
@@ -276,6 +320,7 @@ $(".f228").on("touchstart click", function(){
 			})
 	},700)
 })
+//----------------------------------------//
 $(".f456").on("touchstart click", function(){
 	o.frequency.value = 456;
 	o.connect(ctx.destination);
@@ -289,6 +334,7 @@ $(".f456").on("touchstart click", function(){
 			})
 	},700)
 })
+//----------------------------------------//
 $(".f912").on("touchstart click", function(){
 	o.frequency.value = 912;
 	o.connect(ctx.destination);
@@ -302,52 +348,7 @@ $(".f912").on("touchstart click", function(){
 			})
 	},700)
 })
-//---------------------------------------------------------------//
-$(".min5").on("touchstart click", function(){
-	$(".min5").fadeOut(100);
-	$(".min15").fadeOut(350);
-	$(".min30").fadeOut(700, function(){
-		$(".timerDisplay").fadeIn();
-//--------------------------------------------
-		min5Countdown();
-//--------------------------------------------
-		function stopInterval(){
-			clearInterval(min5StopInverval)
-		}	
-//--------------------------------------------
-	});
-})
-//--------------------------------------------//
-$(".min15").on("touchstart click", function(){
-	$(".min5").fadeOut(100);
-	$(".min15").fadeOut(350);
-	$(".min30").fadeOut(700, function(){
-		$(".timerDisplay").fadeIn();
-//--------------------------------------------
-			min15Countdown();
-//--------------------------------------------
-		function stopInterval(){
-			clearInterval(min5StopInverval)
-		}	
-//--------------------------------------------
-	});
-})
-//--------------------------------------------//
-$(".min30").on("touchstart click", function(){
-	$(".min5").fadeOut(100);
-	$(".min15").fadeOut(350);
-	$(".min30").fadeOut(700, function(){
-		$(".timerDisplay").fadeIn();
-//--------------------------------------------
-		min30Countdown();
-//--------------------------------------------
-		function stopInterval(){
-			clearInterval(min5StopInverval)
-		}	
-//--------------------------------------------
-	});
-})
-//--------------------------------------------//
+//------------------------------------------------------------------------------------//
 $(".firstSacral").on("touchstart click", function(){
 	$(".sacralImage").css("animation", "rotate 999s");
 		$(".firstChakras").animate({opacity: "0"}, function(){
@@ -357,7 +358,7 @@ $(".firstSacral").on("touchstart click", function(){
 				$(".f1212").fadeIn(700);
 	});
 })
-//--------------------------------------------//
+//----------------------------------------//
 $(".f303").on("touchstart click", function(){
 	o.frequency.value = 303;
 	o.connect(ctx.destination);
@@ -398,12 +399,272 @@ $(".f1212").on("touchstart click", function(){
 			})
 	},700)
 })
-//--------------------------------------------//
-
-
-
-
-
+//------------------------------------------------------------------------------------//
+$(".firstSolar").on("touchstart click", function(){
+	$(".solarImage").css("animation", "rotate 999s");
+		$(".firstChakras").animate({opacity: "0"}, function(){
+			$(".solarImage").fadeIn("slow")
+				$(".f182").fadeIn(100);
+				$(".f364").fadeIn(350);
+				$(".f728").fadeIn(700);
+	});
+})
+//----------------------------------------//
+$(".f182").on("touchstart click", function(){
+	o.frequency.value = 182;
+	o.connect(ctx.destination);
+		$(".f364").fadeOut("slow")
+		$(".f728").fadeOut(700)
+		$(".f182").animate({backgroundColor: "red"})
+	setTimeout(function(){
+			$(".f182").animate({bottom: "380px"},700, function(){
+				$(".min5").fadeIn(100);
+				$(".min15").fadeIn(350);
+				$(".min30").fadeIn(700);
+			})
+	},700)
+})
+//----------------------------------------//
+$(".f364").on("touchstart click", function(){
+	o.frequency.value = 364;
+	o.connect(ctx.destination);
+		$(".f182").fadeOut("slow")
+		$(".f728").fadeOut(700)
+	setTimeout(function(){
+			$(".f364").animate({bottom: "380px"},700, function(){
+				$(".min5").fadeIn(100);
+				$(".min15").fadeIn(350);
+				$(".min30").fadeIn(700);
+			})
+	},700)
+})
+//----------------------------------------//
+$(".f728").on("touchstart click", function(){
+	o.frequency.value = 728;
+	o.connect(ctx.destination);
+		$(".f182").fadeOut("slow")
+		$(".f364").fadeOut(700)
+	setTimeout(function(){
+			$(".f728").animate({bottom: "380px"},700, function(){
+				$(".min5").fadeIn(100);
+				$(".min15").fadeIn(350);
+				$(".min30").fadeIn(700);
+			})
+	},700)
+})
+//------------------------------------------------------------------------------------//
+$(".firstHeart").on("touchstart click", function(){
+	$(".heartImage").css("animation", "rotate 999s");
+		$(".firstChakras").animate({opacity: "0"}, function(){
+			$(".heartImage").fadeIn("slow")
+				$(".f128").fadeIn(100);
+				$(".f256").fadeIn(350);
+				$(".f512").fadeIn(700);
+	});
+})
+//----------------------------------------//
+$(".f128").on("touchstart click", function(){
+	o.frequency.value = 128;
+	o.connect(ctx.destination);
+		$(".f256").fadeOut("slow")
+		$(".f512").fadeOut(700)
+		$(".f128").animate({backgroundColor: "red"})
+	setTimeout(function(){
+			$(".f128").animate({bottom: "380px"},700, function(){
+				$(".min5").fadeIn(100);
+				$(".min15").fadeIn(350);
+				$(".min30").fadeIn(700);
+			})
+	},700)
+})
+//----------------------------------------//
+$(".f256").on("touchstart click", function(){
+	o.frequency.value = 256;
+	o.connect(ctx.destination);
+		$(".f128").fadeOut("slow")
+		$(".f512").fadeOut(700)
+	setTimeout(function(){
+			$(".f256").animate({bottom: "380px"},700, function(){
+				$(".min5").fadeIn(100);
+				$(".min15").fadeIn(350);
+				$(".min30").fadeIn(700);
+			})
+	},700)
+})
+//----------------------------------------//
+$(".f512").on("touchstart click", function(){
+	o.frequency.value = 512;
+	o.connect(ctx.destination);
+		$(".f128").fadeOut("slow")
+		$(".f256").fadeOut(700)
+	setTimeout(function(){
+			$(".f512").animate({bottom: "380px"},700, function(){
+				$(".min5").fadeIn(100);
+				$(".min15").fadeIn(350);
+				$(".min30").fadeIn(700);
+			})
+	},700)
+})
+//------------------------------------------------------------------------------------//
+$(".firstThroat").on("touchstart click", function(){
+	$(".throatImage").css("animation", "rotate 999s");
+		$(".firstChakras").animate({opacity: "0"}, function(){
+			$(".throatImage").fadeIn("slow")
+				$(".f192").fadeIn(100);
+				$(".f384").fadeIn(350);
+				$(".f768").fadeIn(700);
+	});
+})
+//----------------------------------------//
+$(".f192").on("touchstart click", function(){
+	o.frequency.value = 192;
+	o.connect(ctx.destination);
+		$(".f384").fadeOut("slow")
+		$(".f768").fadeOut(700)
+		$(".f192").animate({backgroundColor: "red"})
+	setTimeout(function(){
+			$(".f192").animate({bottom: "380px"},700, function(){
+				$(".min5").fadeIn(100);
+				$(".min15").fadeIn(350);
+				$(".min30").fadeIn(700);
+			})
+	},700)
+})
+//----------------------------------------//
+$(".f384").on("touchstart click", function(){
+	o.frequency.value = 384;
+	o.connect(ctx.destination);
+		$(".f192").fadeOut("slow")
+		$(".f768").fadeOut(700)
+	setTimeout(function(){
+			$(".f384").animate({bottom: "380px"},700, function(){
+				$(".min5").fadeIn(100);
+				$(".min15").fadeIn(350);
+				$(".min30").fadeIn(700);
+			})
+	},700)
+})
+//----------------------------------------//
+$(".f768").on("touchstart click", function(){
+	o.frequency.value = 768;
+	o.connect(ctx.destination);
+		$(".f192").fadeOut("slow")
+		$(".f384").fadeOut(700)
+	setTimeout(function(){
+			$(".f768").animate({bottom: "380px"},700, function(){
+				$(".min5").fadeIn(100);
+				$(".min15").fadeIn(350);
+				$(".min30").fadeIn(700);
+			})
+	},700)
+})
+//------------------------------------------------------------------------------------//
+$(".firstThird").on("touchstart click", function(){
+	$(".thirdImage").css("animation", "rotate 999s");
+		$(".firstChakras").animate({opacity: "0"}, function(){
+			$(".thirdImage").fadeIn("slow")
+				$(".f144").fadeIn(100);
+				$(".f288").fadeIn(350);
+				$(".f576").fadeIn(700);
+	});
+})
+//----------------------------------------//
+$(".f144").on("touchstart click", function(){
+	o.frequency.value = 144;
+	o.connect(ctx.destination);
+		$(".f288").fadeOut("slow")
+		$(".f576").fadeOut(700)
+		$(".f144").animate({backgroundColor: "red"})
+	setTimeout(function(){
+			$(".f144").animate({bottom: "380px"},700, function(){
+				$(".min5").fadeIn(100);
+				$(".min15").fadeIn(350);
+				$(".min30").fadeIn(700);
+			})
+	},700)
+})
+//----------------------------------------//
+$(".f288").on("touchstart click", function(){
+	o.frequency.value = 288;
+	o.connect(ctx.destination);
+		$(".f144").fadeOut("slow")
+		$(".f576").fadeOut(700)
+	setTimeout(function(){
+			$(".f288").animate({bottom: "380px"},700, function(){
+				$(".min5").fadeIn(100);
+				$(".min15").fadeIn(350);
+				$(".min30").fadeIn(700);
+			})
+	},700)
+})
+//----------------------------------------//
+$(".f576").on("touchstart click", function(){
+	o.frequency.value = 576;
+	o.connect(ctx.destination);
+		$(".f144").fadeOut("slow")
+		$(".f288").fadeOut(700)
+	setTimeout(function(){
+			$(".f576").animate({bottom: "380px"},700, function(){
+				$(".min5").fadeIn(100);
+				$(".min15").fadeIn(350);
+				$(".min30").fadeIn(700);
+			})
+	},700)
+})
+//------------------------------------------------------------------------------------//
+$(".firstCrown").on("touchstart click", function(){
+	$(".crownImage").css("animation", "rotate 999s");
+		$(".firstChakras").animate({opacity: "0"}, function(){
+			$(".crownImage").fadeIn("slow")
+				$(".f216").fadeIn(100);
+				$(".f432").fadeIn(350);
+				$(".f864").fadeIn(700);
+	});
+})
+//----------------------------------------//
+$(".f216").on("touchstart click", function(){
+	o.frequency.value = 216;
+	o.connect(ctx.destination);
+		$(".f432").fadeOut("slow")
+		$(".f864").fadeOut(700)
+		$(".f216").animate({backgroundColor: "red"})
+	setTimeout(function(){
+			$(".f216").animate({bottom: "380px"},700, function(){
+				$(".min5").fadeIn(100);
+				$(".min15").fadeIn(350);
+				$(".min30").fadeIn(700);
+			})
+	},700)
+})
+//----------------------------------------//
+$(".f432").on("touchstart click", function(){
+	o.frequency.value = 432;
+	o.connect(ctx.destination);
+		$(".f216").fadeOut("slow")
+		$(".f864").fadeOut(700)
+	setTimeout(function(){
+			$(".f432").animate({bottom: "380px"},700, function(){
+				$(".min5").fadeIn(100);
+				$(".min15").fadeIn(350);
+				$(".min30").fadeIn(700);
+			})
+	},700)
+})
+//----------------------------------------//
+$(".f864").on("touchstart click", function(){
+	o.frequency.value = 864;
+	o.connect(ctx.destination);
+		$(".f216").fadeOut("slow")
+		$(".f432").fadeOut(700)
+	setTimeout(function(){
+			$(".f864").animate({bottom: "380px"},700, function(){
+				$(".min5").fadeIn(100);
+				$(".min15").fadeIn(350);
+				$(".min30").fadeIn(700);
+			})
+	},700)
+})
+//------------------------------------------------------------------------------------//
 
 
 
